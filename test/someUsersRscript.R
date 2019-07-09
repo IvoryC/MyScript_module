4bcf7fa4f35775a578728ad19bc23fb2ec9a26b2
Ivory <IvoryEC@gmail.com>
2019-07-09 16:32:58 -0400

    Make a simpler Rscript example.

--- test/someUsersRscript.R
+++ test/someUsersRscript.R
@@ -1,35 +1,13 @@
-
-# This script assumes that it is called sometime after the RarefySeqs module
-# and that the RarefySeqs module was called after SeqFileValidator
-
-getNumReads <- function(filePath){
-  cmd=paste0(c("< ", filePath, " wc -l" ), collapse="")
-  lines=system(cmd, intern=T)
-  numLines = trimws(lines)
-  reads = as.numeric(numLines) / 4
-  return(reads)
-}
-
-getModOutputFiles <- function(modName){
-  modDir = dir("../../", modName)
-  files=dir(paste0(c("../../",modDir, "/output/"),collapse=""), ".f")
-  files=paste0("../../", modDir , "/output/", files)
-  return(files)
-}
-
-seqFileVal.files = getModOutputFiles( "SeqFileValidator" )
-sfv.reads = sapply(seqFileVal.files, getNumReads)
-
-
-rarify.files = getModOutputFiles( "RarefySeqs" )
-rfy.reads = sapply(rarify.files, getNumReads)
+# A toy R script
 
 system("mkdir ../output")
 
-png("../output/ReadsBeforeAfterRarifying.png")
-boxplot(sfv.reads, rfy.reads, las=1, 
+set.seed(2000234)
+
+png("../output/aFigure.png")
+boxplot(rnorm(50, 5), rnorm(50, 7), las=1, 
         col = c("coral", "dodgerblue3"), 
-        names = c("before", "after"),
-        ylab="reads per file")
-title("Rarification")
+        names = c("avg 5", "avg 7"),
+        ylab="random numbers")
+title("Normal Distribution")
 dev.off()
